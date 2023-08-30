dbb10be03ca82a9d378f213981800ec0ee5b1b99
fltramos <fltramos@gmail.com>
2023-08-29 22:00:44 -0300

    Solucao do desafio modelo de dominio e ORM

---
+++ src/main/java/com/devsuperior/sistevento/SisteventoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sistevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisteventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisteventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/sistevento/SisteventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sistevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisteventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
